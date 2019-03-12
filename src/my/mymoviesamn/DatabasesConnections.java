bb88e7a3df2719fe8f6d70734efb1dff30e6be1a
Nektarios K. <nekdevelopment@gmail.com>
2019-03-12 18:27:40 +0200

    μικροδιορθώσεις

--- src/my/mymoviesamn/DatabasesConnections.java
+++ src/my/mymoviesamn/DatabasesConnections.java
@@ -50,49 +50,52 @@ public class DatabasesConnections {
         try {
             Query q = em.createQuery("SELECT f FROM FavoriteList f");
             List<FavoriteList> mFavorites = q.getResultList();
-            return mFavorites;
+            return mFavorites; // επιστροφή λίστας FavoriteList
         } catch (Exception e) {
             System.out.println("Error: Check database connection.");
             return null;
         }
     }
 
-    //Επιστροφή λίστας που περιλαμβάνει το νέο Favorite
+    //Επιστροφή λίστας που περιλαμβάνει το νέο σωσμένο Favorite
     public List<FavoriteList> saveNewFavorite(String newFavoriteName) {
         try {
-            FavoriteList newFavorite = new FavoriteList();
-            newFavorite.setName(newFavoriteName);
+            FavoriteList newFavorite = new FavoriteList(); // νέο αντικείμενο FavoriteList
+            newFavorite.setName(newFavoriteName); // δώσε το όνομα στο νέο FavoriteList
             if (!em.getTransaction().isActive()) {
                 em.getTransaction().begin();
             }
-            em.persist(newFavorite);
+            em.persist(newFavorite); // πρόσθεσε στη ΒΔ το νέο αντικείμενο
             em.merge(newFavorite);
             em.getTransaction().commit();
 
+            // Πάρε τη λίστα με όλα τα FavoriteList 
             Query q = em.createQuery("SELECT f FROM FavoriteList f");
             List<FavoriteList> mFavorites = q.getResultList();
-            return mFavorites;
+            return mFavorites; // επέστρεψε τη νέα λίστα με όλα τα FavoriteList
         } catch (Exception e) {
             System.out.println("Error: Check database connection.");
             return null;
         }
     }
 
-    //Επιστροφή λίστας με αλλαγμένο Favorite από τη database.
+    // Επιστροφή λίστας με αλλαγμένο Favorite από τη database.
     public List<FavoriteList> updateFavorite(String oldName, String newName) {
         try {
             if (!em.getTransaction().isActive()) {
                 em.getTransaction().begin();
             }
+            // Επέλεξε το FavoriteList με το παλιό όνομα oldName
             Query q1 = em.createNativeQuery("SELECT ID FROM FAVORITE_LIST WHERE NAME= '" + oldName + "'");
-            List r1 = q1.getResultList();
-            FavoriteList fl1 = em.find(FavoriteList.class, r1.get(0));
-            fl1.setName(newName);
+            List r1 = q1.getResultList(); // Πάρε τη λίστα του αποτελέσματος του query
+            FavoriteList fl1 = em.find(FavoriteList.class, r1.get(0)); // Πάρε το 1ο FavoriteList της λίστας
+            fl1.setName(newName); // όρισε το νέο όνομα του favoriteList
             em.getTransaction().commit();
 
+            // Πάρε τη λίστα με όλα τα FavoriteList 
             Query q = em.createQuery("SELECT f FROM FavoriteList f");
             List<FavoriteList> mFavorites = q.getResultList();
-            return mFavorites;
+            return mFavorites; // επέστρεψε τη νέα λίστα με όλα τα FavoriteList
         } catch (Exception e) {
             System.out.println("Error: Check database connection.");
             return null;
@@ -106,26 +109,30 @@ public class DatabasesConnections {
                 em.getTransaction().begin();
             }
 
+            // εκτέλεσε βρόγχο επανάληψης ανάμεσα σε όλα τα επιλεγμένα favoriteLists
             for (int i = 0; i < selectedNames.size(); i++) {
 
                 // Αποσυσχέτιση ταινιών από τη συγκεκριμένη Favorite List (i)
+                // query λήψης του id της favoriteList που επιλέχθηκε
                 Query queryMovies = em.createNativeQuery("SELECT ID FROM FAVORITE_LIST WHERE NAME= '" + selectedNames.get(i) + "'");
                 List moviesList = queryMovies.getResultList();
-                String stringId = moviesList.get(0).toString();
+                String stringId = moviesList.get(0).toString(); // πάρε το id σε μορφή string
+                // query λήψης όλων των ταινιών με το συγκεκριμένο id στο favorite_list_id
                 Query q1 = em.createQuery("SELECT m FROM Movie m WHERE m.favoriteListId.id = " + stringId);
                 List<Movie> moviesToChange = q1.getResultList();
                 for (Movie movie : moviesToChange) {
-                    movie.setFavoriteListId(null);
+                    movie.setFavoriteListId(null); // κάνε null το favorite_list_id τηε ταινίας (αποσυσχέτιση)
                 }
 
                 // Τώρα Διαγραφή Λίστας Αγαπημένων
                 Query query = em.createNativeQuery("SELECT ID FROM FAVORITE_LIST WHERE NAME= '" + selectedNames.get(i) + "'");
                 List results = query.getResultList();
                 FavoriteList fl = em.find(FavoriteList.class, results.get(0));
-                em.remove(fl);
+                em.remove(fl); // διαγραφή FavoriteList fl
             }
             em.getTransaction().commit();
 
+            // Λήψη και επιστροφή της νέας λίστας FavoriteList για εμφάνιση
             Query q = em.createQuery("SELECT f FROM FavoriteList f");
             List<FavoriteList> mFavorites = q.getResultList();
             return mFavorites;
@@ -169,6 +176,7 @@ public class DatabasesConnections {
         String localURL = "genre/movie/list?";
         String resultGenres = readFromURL(BASE_URL + localURL + AMN_API_KEY);
 
+        // προσπάθεια ανάγνωσης του json αρχείου
         try {
             JSONObject response = new JSONObject(resultGenres);
             JSONArray genres = response.optJSONArray("genres");
@@ -178,14 +186,14 @@ public class DatabasesConnections {
                 genre = new Genre();
                 genre.setId(aGenreObject.getInt("id"));
                 genre.setName(aGenreObject.getString("name"));
+                // εάν το genreId είναι 28 ή 10749 ή 878 κάνε τα παρακάτω
                 if (aGenreObject.getInt("id") == 28 || aGenreObject.getInt("id") == 10749 || aGenreObject.getInt("id") == 878) {
                     if (!em.getTransaction().isActive()) {
                         em.getTransaction().begin();
                     }
-                    em.merge(genre);
+                    em.merge(genre); // πρόσθεσε το genre στον πίνακα Genre
                     em.flush();
                     em.getTransaction().commit();
-//                    System.out.println("id= " + genre.getId() + ", name= " + genre.getName());
                 }
             }
         } catch (JSONException ex) {
@@ -194,6 +202,7 @@ public class DatabasesConnections {
     }
 // Διαγραφή της Βάσης Δεδομένων
     public void clearDatabase() {
+        // queries αδειάζματος πινάκων της ΒΔ
         Query deleteFavorites = em.createQuery("DELETE FROM FavoriteList");
         Query deleteMovies = em.createQuery("DELETE FROM Movie");
         Query deleteGenres = em.createQuery("DELETE FROM Genre");
@@ -202,11 +211,11 @@ public class DatabasesConnections {
             em.getTransaction().begin();
         }
 
-        deleteMovies.executeUpdate();
+        deleteMovies.executeUpdate(); // Διαγραφή (άδειασμα) πίνακα Movie
         System.out.println("Movie table deleted");
-        deleteFavorites.executeUpdate();
+        deleteFavorites.executeUpdate(); // Διαγραφή (άδειασμα) πίνακα FavoriteList
         System.out.println("Favorite list table deleted");
-        deleteGenres.executeUpdate();
+        deleteGenres.executeUpdate(); // Διαγραφή (άδειασμα) πίνακα Genre
         System.out.println("Genres table deleted");
 
         em.flush();
@@ -216,20 +225,19 @@ public class DatabasesConnections {
     // Λήψη δεδομένων ταινιών και αποθήκευση στον πίνακα MOVIES
     public boolean getMovies(MainFrame aThis) {
 
-        // Αρχικές ρυθμίσεις για το παράθυρο downloading...
+        // Αρχικές ρυθμίσεις για το παράθυρο downloading f...
         JWindow f = new JWindow();
         f.setAlwaysOnTop(true);
-//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container content = f.getContentPane();
-        JProgressBar progressBar = new JProgressBar();
-        progressBar.setValue(0);
+        Container content = f.getContentPane(); // λήψη του container του f
+        JProgressBar progressBar = new JProgressBar(); // δημιουργία JProgressBar
+        progressBar.setValue(0); // αρχική τιμή progressBar
         progressBar.setStringPainted(true);
         Border border = BorderFactory.createTitledBorder("Downloading...");
         progressBar.setBorder(border);
-        content.add(progressBar);
-        f.setSize(300, 60);
+        content.add(progressBar); // προσθήκη progressBar στο content
+        f.setSize(300, 60); // ορισμός μεγέθους f
         f.setLocationRelativeTo(null);
-        f.setVisible(true);
+        f.setVisible(true); // κάνε ορατό το f
 
         // Υπολογίζω τον συνολικό αριθμό των σελίδων του αποτελέσματος
 //        int numberOfPages = 1;
@@ -241,29 +249,36 @@ public class DatabasesConnections {
 //        } catch (JSONException ex) {
 //            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
 //        }
-        int maxpages = 10;
 
+
+        int maxpages = 10; // αριθμός σελίδων (pages) που θα γίνουν download
+
+        // βρόγχος διαβάσματος κάθε σελίδας 
         for (int m = 1; m <= maxpages; m++) {
 
             // ενημέρωση progressBar downloading
             progressBar.setValue((100 / maxpages) * m);
             progressBar.update(progressBar.getGraphics());
 
+            // το κείμενο της σελίδας m που διαβάζω
             String resultPerPage = readFromURL(BASE_URL + "discover/movie?with_genres=28|10749|878&primary_release_date.gte=2000-01-01&" + AMN_API_KEY + "&language=el" + "&page=" + m);
             System.out.println("resultPerPage= " + resultPerPage);
 
+            // προσπάθεια διαβάσματος αρχείου json και ανάκτησης απαραίτητων πληροφοριών
+            // για δημιουργία και σώσιμο στην ΒΔ αντικειμένου Movie (item)
             try {
                 JSONObject response = new JSONObject(resultPerPage);
 
                 JSONArray results = response.optJSONArray("results");
-                Movie item;
+                Movie item; // αντικείμενο Movie
 
                 for (int i = 0; i < results.length(); i++) {
                     JSONObject aMovieObject = results.optJSONObject(i);
                     item = new Movie();
-                    item.setId(aMovieObject.getInt("id"));
-                    item.setTitle(aMovieObject.getString("title"));
+                    item.setId(aMovieObject.getInt("id")); // διάβασε και δώσε το id στο αντικείμενο item
+                    item.setTitle(aMovieObject.getString("title")); // διάβασε και δώσε το title στο αντικείμενο item
 
+                    // Ανάλογα τον αριθμό του genre_ids όρισε το Genre του αντικειμένου item
                     JSONArray genre_ids = aMovieObject.optJSONArray("genre_ids");
                     OUTER:
                     for (int k = 0; k < genre_ids.length(); k++) {
@@ -282,29 +297,28 @@ public class DatabasesConnections {
                         }
                     }
 
+                    // Διάβασε τη release_date και κάνε την string
                     String releaseDateString = aMovieObject.getString("release_date");
                     DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
                     Date releaseDate = new Date();
                     try {
                         releaseDate = df.parse(releaseDateString);
-//                    String newDateString = df.format(releaseDate);
-//                    System.out.println(newDateString);
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
-                    item.setReleaseDate(releaseDate);
-                    item.setRating(aMovieObject.getFloat("vote_average"));
-                    String s = aMovieObject.getString("overview");
-                    if (s.length() >= 500) {
-                        s = s.substring(0, 499);
+                    item.setReleaseDate(releaseDate); // διάβασε και δώσε το releaseDate στο item
+                    item.setRating(aMovieObject.getFloat("vote_average")); // διάβασε και δώσε το vote_average στο item
+                    String s = aMovieObject.getString("overview"); // διάβασε το overview
+                    if (s.length() >= 500) { // αν το μήκος είναι μεγαλύτερο από 500 χαρακτήρες
+                        s = s.substring(0, 499); // κράτησε τους 500 πρώτους χαρακτήρες
                     }
 
-                    item.setOverview(s);
+                    item.setOverview(s); // δώσε το overview στο item
                     if (!em.getTransaction().isActive()) {
                         em.getTransaction().begin();
                     }
 
-                    em.merge(item);
+                    em.merge(item); // πρόσθεσε το item στον πίνακα Movie
                     em.flush();
                     em.getTransaction().commit();
                 }
@@ -330,7 +344,7 @@ public class DatabasesConnections {
         String stringId = results.get(0).toString();
         Query q1 = em.createQuery("SELECT m FROM Movie m WHERE m.favoriteListId.id = " + stringId);
 
-        return q1.getResultList();
+        return q1.getResultList(); // επιστροφή της λίστας των ταινιών 
     }
 
 }
